2947841838a76253fdcf586f58b5d968fc637c13
NaveenGP2005 <naveengpatil26@gmail.com>
2026-04-24 22:08:32 +0530

    Add Ichihime and Triangle [A] from Codeforces

---
+++ 1337/A - Ichihime and Triangle/solution.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin>>t;
+    int a[4];
+    while(t--){
+        for(int i=0;i<4;i++) cin>>a[i];
+        cout<<a[0]<<" "<<a[2]<<" "<<a[2]<<endl;
+    }
+    return 0;
+}
