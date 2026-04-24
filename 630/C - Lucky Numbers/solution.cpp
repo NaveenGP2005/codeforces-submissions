3368250d03f0bb3f08feefb995834917d50650ba
NaveenGP2005 <naveengpatil26@gmail.com>
2026-04-24 22:15:31 +0530

    Add Lucky Numbers [C] from Codeforces

---
+++ 630/C - Lucky Numbers/solution.cpp
@@ -0,0 +1,12 @@
+// Online C++ compiler to run C++ program online
+#include <bits/stdc++.h>
+#include<algorithm>
+using namespace std;
+ 
+int main() {
+   long long n;
+   cin>>n;
+   cout<<(1LL<<(n+1))-2<<endl;
+   
+   return 0;
+}
