8e613019d1116255d71e15665093a7866edbcf26
NaveenGP2005 <naveengpatil26@gmail.com>
2026-04-24 22:14:01 +0530

    Add Magic Stick [B] from Codeforces

---
+++ 1257/B - Magic Stick/solution.cpp
@@ -0,0 +1,17 @@
+// Online C++ compiler to run C++ program online
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y;
+        cin>>x>>y;
+        if(x==1&&y>1)
+        cout<<"NO"<<endl;
+        else if(x<=3&&y>3)
+        cout<<"NO"<<endl;
+        else            cout<<"YES"<<endl;
+    }
+    return 0;
+    }
