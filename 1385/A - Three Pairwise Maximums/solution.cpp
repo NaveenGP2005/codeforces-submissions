d7181d4626ad6e723bef24bd83e2ddc3b6b61152
NaveenGP2005 <naveengpatil26@gmail.com>
2026-04-24 22:10:22 +0530

    Add Three Pairwise Maximums [A] from Codeforces

---
+++ 1385/A - Three Pairwise Maximums/solution.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        vector<long long>a(3);
+        for(int i=0;i<3;i++) cin>>a[i];
+        sort(a.begin(),a.end());
+        if(a[1]!=a[2]) cout<<"NO"<<endl;
+        else cout<<"YES"<<endl<<a[0]<<" "<<a[0]<<" "<<a[2]<<endl;
+    }
+    return 0;
+}
