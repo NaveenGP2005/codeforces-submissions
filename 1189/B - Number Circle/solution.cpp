fea3c0b84b1c86d65606915e46bffc1b9fb5516b
NaveenGP2005 <naveengpatil26@gmail.com>
2026-04-24 22:15:36 +0530

    Add Number Circle [B] from Codeforces

---
+++ 1189/B - Number Circle/solution.cpp
@@ -0,0 +1,23 @@
+// Online C++ compiler to run C++ program online
+#include <bits/stdc++.h>
+#include<algorithm>
+using namespace std;
+ 
+int main() {
+   long long n;
+   cin>>n;
+   vector<long long>a(n);
+   for(int i=0;i<n;i++) cin>>a[i];
+   sort(a.begin(),a.end());
+    if(a[n-3]+a[n-2] <=a[n-1])cout<<"NO"<<endl;
+    else
+    {
+        cout<<"YES"<<endl;
+        cout<<a[n-1]<<" ";
+        for(int i=n-3;i>=0;i-- )cout<<a[i]<<" ";
+        cout<<a[n-2]<<endl;
+    }
+   
+   
+   return 0;
+}
