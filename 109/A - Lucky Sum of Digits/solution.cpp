46629402a09b3f8137460b4c2129dd7360f0575f
NaveenGP2005 <naveengpatil26@gmail.com>
2026-04-24 22:07:59 +0530

    Add Lucky Sum of Digits [A] from Codeforces

---
+++ 109/A - Lucky Sum of Digits/solution.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int n;
+    cin>>n;
+    string s="";
+    while(n>0&&n%7!=0) n-=4,s.push_back('4');
+    while(n>0&&n%7==0) n-=7,s.push_back('7');
+    if(n==0) cout<<s<<endl;
+    else cout<<-1<<endl;
+    return 0;
+}
