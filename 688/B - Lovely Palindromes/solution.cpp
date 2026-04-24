efab25549de56974f0a45b707888e6b8d25cb8d2
NaveenGP2005 <naveengpatil26@gmail.com>
2026-04-24 22:15:20 +0530

    Add Lovely Palindromes [B] from Codeforces

---
+++ 688/B - Lovely Palindromes/solution.cpp
@@ -0,0 +1,13 @@
+// Online C++ compiler to run C++ program online
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+   string n;
+   cin>>n;
+   cout<<n;
+   for(int i=n.length()-1;i>=0;i--) cout<<n[i];
+   cout<<endl;
+   
+   return 0;
+}
