bf862a389e5f1be6c0db4eff6f4d6abc75f1af3c
NaveenGP2005 <naveengpatil26@gmail.com>
2026-04-24 22:14:11 +0530

    Add Digital root [B] from Codeforces

---
+++ 1107/B - Digital root/solution.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        long long k,n;
+        cin>>k>>n;
+        cout<<n+(k-1)*9<<endl;
+    }
+    return 0;
+}
