ab9d85c1a78e78df603abb2ffff8fd1edb7a7d21
NaveenGP2005 <naveengpatil26@gmail.com>
2026-04-24 22:14:22 +0530

    Add Theatre Square [A] from Codeforces

---
+++ 1/A - Theatre Square/solution.cpp
@@ -0,0 +1,9 @@
+// Online C++ compiler to run C++ program online
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    long long m,n,a;
+    cin>>m>>n>>a;
+    cout<<((m+a-1)/a)*((n+a-1)/a)<<endl;
+    return 0;
+    }
