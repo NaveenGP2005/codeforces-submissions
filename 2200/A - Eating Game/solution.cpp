a4f10fa26b637193d7a707a2d8d902c496ecc108
NaveenGP2005 <naveengpatil26@gmail.com>
2026-04-24 22:12:53 +0530

    Add Eating Game [A] from Codeforces

---
+++ 2200/A - Eating Game/solution.cpp
@@ -0,0 +1,20 @@
+// Online C++ compiler to run C++ program online
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int>a(n);
+        for(int i=0;i<n;i++) cin>>a[i];
+        
+        int ans=*max_element(a.begin(),a.end());
+        int cnt=0;
+        for(int i=0;i<n;i++)
+        if(ans==a[i]) cnt++;
+        cout<<cnt<<endl;
+    }
+    return 0;
+}
