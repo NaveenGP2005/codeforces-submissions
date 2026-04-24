4f45848cbcdfe50b1b4c2cc51e43e12627189ce0
NaveenGP2005 <naveengpatil26@gmail.com>
2026-04-24 22:07:46 +0530

    Add Chocolates [B] from Codeforces

---
+++ 1139/B - Chocolates/solution.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int n;
+    cin >> n;
+ 
+    vector<long long> a(n);
+    for (int i = 0; i < n; i++) cin >> a[i];
+    long long ans=a[n-1];
+    for (int i = n - 2; i >= 0; i--){
+  
+  		if (a[i] < a[i + 1]) {
+   			ans += a[i];
+  		}
+  		else {
+   			a[i] = a[i + 1] - 1;
+    			ans += a[i];
+  		}
+  		if (a[i] == 0)break;
+ 	}
+ 
+    cout << ans << endl;
+    return 0;
+}
