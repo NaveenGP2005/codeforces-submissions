f4cd980ecd0710abf9efaedd523bb7fc37ed9c71
Naveen G Patil <naveengpatil26@gmail.com>
2026-07-14 20:30:22 +0530

    Add Nikita and Books [B] from Codeforces

---
+++ 2244/B - Nikita and Books/solution.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+const int MAX=9000005;
+int trie[MAX][2];
+int cnt[MAX];
+int ncnt=1;
+ 
+void insert(int x){
+    int curr=1;
+    for(int i=29;i>=0;i--){
+        int bit=(x>>i)&1;
+        if(trie[curr][bit]==0){
+            trie[curr][bit]=++ncnt;
+        }
+        
+        curr=trie[curr][bit];
+        cnt[curr]++;
+    }
+}
+ 
+void remove(int x){
+    int curr=1;
+    for(int i=29;i>=0;i--){
+        int bit=(x>>i)&1;
+        curr=trie[curr][bit];
+        cnt[curr]--;
+    }
+}
+ 
+int query(int x){
+    int curr=1;
+    int best=0;
+    for(int i=29;i>=0;i--){
+        int bit=(x>>i)&1;
+        if(trie[curr][bit]!=0&&cnt[trie[curr][bit]]>0){
+            best|=(bit<<i);
+            curr=trie[curr][bit];
+        }else{
+            int ob=1-bit;
+            best|=(ob<<i);
+            curr=trie[curr][ob];
+        }
+}
+        return best;
+}
+ 
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+ 
+   /*int n;
+    cin>>n;
+    vector<int>a(n);
+    for(int i=0;i<n;i++) cin>>a[i];
+    
+    for(int i=0;i<n;i++){
+        int p;
+        cin>>p;
+        insert(p);
+    }
+    
+    for(int i=0;i<n;i++){
+        int best=query(a[i]);
+        cout<<(a[i]^best)<<(i==n-1?"":" ");
+        remove(best);
+    }
+    cout<<endl;*/
+    
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        long long sum=0;
+        bool pos=1;
+        
+        for(long long i=1;i<=n;i++) {
+            long long x;
+            cin>>x;
+            
+            sum+=x;
+            
+            long long ex=(i*(i+1))/2;
+            if(sum<ex) pos=0;
+        }
+        
+        if(pos) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+    
+    return 0;
+}
